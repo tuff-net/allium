38b33f8728fed44ee838407b24c4106ce5f1be69
hugeblank <hugeblank@gmail.com>
2025-03-09 18:45:08 -0700

    chore: prepare bouquet for relase

--- bouquet/examples/hangman/main.lua
+++ bouquet/examples/hangman/main.lua
@@ -6,13 +6,11 @@
 
 local words = require("words")
 local CommandManager = require("net.minecraft.server.command.CommandManager") -- We need the java command manager for creating commands.
-local MessageType = require("net.minecraft.network.message.MessageType") -- "net.minecraft." among other packages are auto-filled for you!
 local arguments = command.arguments -- Create shortcut for command argument types
 
-local word
-local guessed
-local guesses = 0
-local function parseGuess()
+local active = {}
+
+local function parseGuess(word, guessed)
     -- Returns a string with underscores where a letter hasn't been guessed
     local out = ""
     for i = 1, #word do
@@ -28,13 +26,12 @@ local function parseGuess()
     return out
 end
 
-local function broadcast(context, text) -- easily broadcast a message to all players
+local function sendMessage(context, data) -- easily broadcast a message to all players
+    local out = text.format(data)
     context
             :getSource()
-            :getWorld()
-            :getServer()
-            :getPlayerManager()
-            :broadcast(texts.parseSafe(text), MessageType.SYSTEM)
+            :getPlayer()
+            :sendMessage(out, false)
 end
 
 local builder = CommandManager.literal("hangman") -- Create the builder for the hangman command
@@ -48,88 +45,106 @@ events.server.COMMAND_REGISTER:register(script, function(_, name, success)
     end
 end)
 
-builder:executes(function(context) -- The part of the command with no values attached
-    if word ~= nil then -- If there's a game being played tell the player to guess
-        context:getSource():sendFeedback(texts.parseSafe("<red>No guess! Add a letter or word to guess</red>"), false)
+builder:executes(function(context)
+    local out = {
+        "A hangman game written entirely in Lua using <url 'https://github.com/moongardenmods/allium'>",
+        "<hover 'Click to view on GitHub'><light_purple>All</light_purple><dark_purple>i</dark_purple>",
+        "<light_purple>um</light_purple> & <rainbow>Bouquet</rainbow></hover></url>. Run <gray>",
+        "<cmd '/hangman start'><hover 'Click to suggest'>/hangman start</hover></cmd></gray> to start, ",
+        "guess with <gray><cmd '/hangman guess'><hover 'Click to suggest'>/hangman guess <letter|word>",
+        "</hover></cmd></gray>."
+    }
+    sendMessage(context, table.concat(out))
+    return 1
+end)
+
+builder:m_then(CommandManager.literal("start"):executes(function(context) -- The part of the command with no values attached
+    local player = context:getSource():getPlayer()
+    if active[player] then -- If there's a game being played tell the player to guess
+        context:getSource():sendError(text.format("No guess! Add a letter or word to guess"))
         return 0 -- Execution handlers expect an integer return value.
         -- We use 0 to indicate error, and 1 to indicate success.
     else -- Start a game, since there's not one currently playing
-        word = java.split(words[math.random(1, #words)], "")
-        guessed = {} -- Create a table to mark guessed characters in the word
-        for i = 1, #word do
-        guessed[i] = false
+        local game = {
+            guesses = 10, -- Give 10 guesses. Could be increased to reduce the difficulty.
+            guessed = {}, -- Create a table to mark guessed characters in the word
+            word = {}
+        }
+        local target = words[math.random(1, #words)]
+        for i = 1, target:len() do
+            game.word[i] = target:sub(i,i)
+            game.guessed[i] = false
         end
-        guesses = 10 -- Give 10 guesses. Could be increased to reduce the difficulty.
-        broadcast(context, string.format(
+        active[player] = game
+        sendMessage(context, string.format(
                 "Guess the word!\n<bold>%s</bold>\nYou have %d guesses. Good luck!",
-                parseGuess(),
-                guesses
+                parseGuess(game.word, game.guessed),
+                game.guesses
         ))
         return 1
     end
-end)
+end))
 
-builder:m_then(CommandManager.argument("guess", arguments.string.word()):executes(function(context)
+builder:m_then(CommandManager.literal("guess")
+                             :m_then(CommandManager.argument("guess", arguments.string.word())
+                                                   :executes(function(context)
     -- The part of the command that handles guesses
-    local playerName = context:getSource():getPlayer():getName():getString()
-    local broadcastWin = function() -- easily broadcast win message
-        broadcast(context, string.format(
-                        "<green>%s guessed the word! It was: <bold>%s</bold></green>",
-                        playerName,
-                        table.concat(word, "")
-                ))
-        word = nil -- Clear the word, ending the game
-        guesses = 0
+    local player = context:getSource():getPlayer()
+    local game = active[player]
+    local sendWin = function() -- easily broadcast win message
+        sendMessage(context, string.format(
+                "<green>You guessed the word! It was: <bold>%s</bold></green>",
+                table.concat(game.word, "")
+        ))
+        active[player] = nil
         return 1
     end
     local str = arguments.string.getString(context, "guess") -- Get the guess from the command context
-    if word ~= nil then -- If theres a game running
-        broadcast(context, playerName.." guessed <bold>"..str.."</bold>")
+    if game then -- If theres a game running
+        sendMessage(context, "You guessed <bold>"..str.."</bold>")
         if #str == 1 then -- If the guess is a letter
             local correct = false
             local total = 0 -- Keep track of the total number of letters guessed so far
-            for i = 1, #word do -- Check the word for the letter
-                if word[i] == str and not guessed[i] then -- If there's a new match
+            for i = 1, #game.word do -- Check the word for the letter
+                if game.word[i] == str and not game.guessed[i] then -- If there's a new match
                     correct = true -- Mark the guess as correct
-                    guessed[i] = true -- Mark all letters in the word that match as guessed
+                    game.guessed[i] = true -- Mark all letters in the word that match as guessed
                 end
-                if guessed[i] then total = total + 1 end -- increment total if the current letter has been guessed
+                if game.guessed[i] then total = total + 1 end -- increment total if the current letter has been guessed
             end
-            if total == #word then -- If all letters have been guessed
-                return broadcastWin()
+            if total == #game.word then -- If all letters have been guessed
+                return sendWin()
             elseif correct then -- If the guess was marked as correct
-                broadcast(context, "<green>"..playerName.." guessed a letter correctly!</green>")
+                sendMessage(context, "<green>You guessed a letter correctly!</green>")
             else
-                broadcast(context, "<red>"..playerName.." guessed a letter incorrectly!</red>")
-                guesses = guesses-1 -- Subtract a guess
+                sendMessage(context, "<red>You guessed a letter incorrectly!</red>")
+                game.guesses = game.guesses-1 -- Subtract a guess
             end
         else -- If the guess is a word
-            if str == table.concat(word, "") then -- If the guessed word is an exact match
-                return broadcastWin()
+            if str == table.concat(game.word, "") then -- If the guessed word is an exact match
+                return sendWin()
             else -- Otherwise the guess is incorrect
-                broadcast(context, "<red>"..playerName.." guessed incorrectly! </red>")
-                guesses = guesses-1 -- Subtract a guess
+                sendMessage(context, "<red>You guessed incorrectly! </red>")
+                game.guesses = game.guesses-1 -- Subtract a guess
             end
         end
-        if guesses > 0 then -- So long as the game has guesses left
-            broadcast(context, "<bold>"..parseGuess().."</bold>")
+        if game.guesses > 0 then -- So long as the game has guesses left
+            sendMessage(context, "<bold>"..parseGuess(game.word, game.guessed).."</bold>")
             local s = " guesses"
-            if guesses == 1 then s = " guess" end -- Handle the English language
-            broadcast(context, tostring(guesses)..s.." left")
+            if game.guesses == 1 then s = " guess" end -- Handle the English language
+            sendMessage(context, tostring(game.guesses)..s.." left")
         else -- No guesses left, game over!
-            broadcast(context, string.format(
+            sendMessage(context, string.format(
                     "<red><bold>Game over!</bold> The word was: <bold>%s</bold></red>",
-                    table.concat(word, "")
+                    table.concat(game.word, "")
             ))
-            word = nil
+            active[player] = nil
         end
     else -- No game, tell player how to start one
-        context:getSource():sendFeedback(
-                texts.parseSafe("<red>No game! Use /hangman with no guess to start</red>"),
-                false
-        )
+        context:getSource():sendError(text.format("No game! <gray>Use /hangman start</gray> to play!"))
     end
     return 1
-end))
+end)
+))
 
 command.register(builder) -- Register the command
